8c6da8159b08ca0bc950e9b2badf6c515da5f603
innasolp <innasolp@gmail.com>
2025-05-13 14:34:48 +0300

    LOGGING-003 fixed

--- SeriLog.Configuration.Extensions/ConfigurationExtensions.cs
+++ SeriLog.Configuration.Extensions/ConfigurationExtensions.cs
@@ -8,26 +8,25 @@ public static class ConfigurationExtensions
     internal const string SourceContextStr = "$[SourceContext]";
 
     internal const string PropertyNameContextStr = "$[PropertyName]";
+    internal const string ContextPropertyNameContextStr = "$[ContextPropertyName]";
     internal const string PropertyValueContextStr = "$[PropertyValue]";
     internal const string FuncContextStr = "$[Func]";
     internal const string And = "and";
-    
+
+    internal const string ConfigureLoggerSectionName = "configureLogger";
+    internal const string FilterSectionName = "Filter";
+    internal const string ExpressionSectionName = "expression";
+    internal const string WriteToSectionName = "WriteTo";
+    internal const string OutputTemplateSectionName = "outputTemplate";
+    internal const string PropertyNameSectionName = "propertyName";
 
     public static bool SetSerilogLoggersFilterSourceContext(this IConfiguration configuration, string context)
     {
-        var writeToSections = configuration.GetWriteToSections();
-        if (writeToSections.Count == 0) return false;
+        var expressionSections = configuration.GetWriteToExpressionSections().Where(e => e.Value != null && e.Value.Contains(SourceContextStr));
 
-        foreach (var writeTo in writeToSections)
+        foreach (var expression in expressionSections)
         {
-            var expressions = writeTo.GetSection("Args:configureLogger:Filter").GetChildren()
-                .Select(c => c.GetSection("Args:expression"))
-                .Where(e => e.Value != null && e.Value.Contains(SourceContextStr)).ToList();
-
-            foreach (var expression in expressions)
-            {
-                expression.Value = expression.Value?.Replace(SourceContextStr, context);
-            }
+            expression.Value = expression.Value?.Replace(SourceContextStr, context);            
         }
 
         return true;
@@ -35,27 +34,23 @@ public static class ConfigurationExtensions
 
     public static bool SetSerilogLoggersFilterFunc(this IConfiguration configuration, string? func, string? propertyName, object? propertyValue)
     {
-        if(string.IsNullOrEmpty(func) && string.IsNullOrEmpty(propertyName) && propertyValue == null) return false;
-       
+        if (string.IsNullOrEmpty(func) && string.IsNullOrEmpty(propertyName) && propertyValue == null) return false;
+
         var writeToSections = configuration.GetWriteToSections();
         if (writeToSections.Count == 0) return false;
 
-        foreach (var writeTo in writeToSections)
-        {
-            var expressions = writeTo.GetSection("Args:configureLogger:Filter").GetChildren()
-                .Select(c => c.GetSection("Args:expression"))
-                .Where(e => e.Value != null &&
-                ( (!string.IsNullOrEmpty(func) && !e.Value.Contains(FuncContextStr))
+        var expressionSections = configuration.GetWriteToExpressionSections().Where(e => e.Value != null &&
+                ((!string.IsNullOrEmpty(func) && !e.Value.Contains(FuncContextStr))
                 || (!string.IsNullOrEmpty(propertyName) && !e.Value.Contains(PropertyNameContextStr))
-                || (propertyValue != null && e.Value.Contains(PropertyValueContextStr)) )).ToList();
+                || (propertyValue != null && e.Value.Contains(PropertyValueContextStr))));
 
-            foreach (var expression in expressions)
-            {
-                expression.Value = expression.Value?
-                    .Replace(FuncContextStr, !string.IsNullOrEmpty(func) ? func : "")
-                    .Replace(PropertyNameContextStr, !string.IsNullOrEmpty(propertyName) ? propertyName : "")
-                    .Replace(PropertyValueContextStr, propertyValue != null ? propertyValue.ToString() : "");
-            }
+
+        foreach (var expression in expressionSections)
+        {
+            expression.Value = expression.Value?
+                .Replace(FuncContextStr, !string.IsNullOrEmpty(func) ? func : "")
+                .Replace(PropertyNameContextStr, !string.IsNullOrEmpty(propertyName) ? propertyName : "")
+                .Replace(PropertyValueContextStr, propertyValue != null ? propertyValue.ToString() : "");
         }
 
         return true;
@@ -63,19 +58,11 @@ public static class ConfigurationExtensions
 
     public static bool SetSerilogOutputTemplateProperty(this IConfiguration configuration, string propertyName)
     {
-        var writeToSections = configuration.GetWriteToSections();
-        if (writeToSections.Count == 0) return false;
+        var outputTemplateSections = configuration.GetWriteToOutputTemplateSections().Where(e => e.Value != null && e.Value.Contains(PropertyNameContextStr));
 
-        foreach (var writeTo in writeToSections)
+        foreach (var expression in outputTemplateSections)
         {
-            var pathes = writeTo.GetSection("Args:configureLogger:WriteTo").GetChildren()
-                .Select(c => c.GetSection("Args:outputTemplate"))
-               .Where(e => e.Value != null && e.Value.Contains(PropertyNameContextStr)).ToList();
-
-            foreach (var expression in pathes)
-            {
-                expression.Value = expression.Value?.Replace(PropertyNameContextStr, propertyName);
-            }
+            expression.Value = expression.Value?.Replace(PropertyNameContextStr, propertyName);
         }
 
         return true;
@@ -129,47 +116,113 @@ public static class ConfigurationExtensions
     }
 
     internal const string LoggerPathStr = "$[LoggerPath]";
+    public static bool SetSerilogLoggersPath(this IConfiguration configuration, string[] pathSectionNames, string path)
+    {
+        var pathSections = configuration.GetWriteToPathSections(pathSectionNames);
+        if (pathSections.Count == 0) return false;
+
+        foreach (var pathSection in pathSections.Where(e => e.Value != null && e.Value.Contains(LoggerPathStr)))
+        {
+            pathSection.Value = pathSection.Value?.Replace(LoggerPathStr, path);
+        }
+
+        return true;
+    }
+
     public static bool SetSerilogLoggersPath(this IConfiguration configuration, string path)
+    {
+        return SetSerilogLoggersPath(configuration, ["path"], path);
+    }
+
+    public static bool SetSerilogWriteToContextPropertyName(this IConfiguration configuration, string contextPropertyName)
     {
         var writeToSections = configuration.GetWriteToSections();
         if (writeToSections.Count == 0) return false;
 
         foreach (var writeTo in writeToSections)
         {
-            var pathes = writeTo.GetSection("Args:configureLogger:WriteTo").GetChildren()
-                .Select(c => c.GetSection("Args:path"))
-                .Where(e => e.Value != null && e.Value.Contains(LoggerPathStr)).ToList();
+            foreach (var expression in writeTo.GetWriteToExpressionSections().Where(s=>s.Value?.Contains(ContextPropertyNameContextStr) == true))
+                expression.Value = expression?.Value?.Replace(ContextPropertyNameContextStr, contextPropertyName);
 
-            foreach (var expression in pathes)
-            {
-                expression.Value = expression.Value?.Replace(LoggerPathStr, path);
-            }
+            foreach (var outputTemplate in writeTo.GetWriteToOutputTemplateSections().Where(s => s.Value?.Contains(ContextPropertyNameContextStr) == true))
+                outputTemplate.Value = outputTemplate?.Value?.Replace(ContextPropertyNameContextStr, contextPropertyName);
+
+            foreach (var propertyName in writeTo.GetWriteToSectionsByName(PropertyNameSectionName).Where(s => s.Value?.Contains(ContextPropertyNameContextStr) == true))
+                propertyName.Value = propertyName.Value?.Replace(ContextPropertyNameContextStr, contextPropertyName);
+
+            foreach (var path in writeTo.GetWriteToPathSections(["path", "pathFormat"]).Where(s => s.Value?.Contains(ContextPropertyNameContextStr) == true))
+                path.Value = path?.Value?.Replace(ContextPropertyNameContextStr, contextPropertyName);
         }
 
-        return true;
+       return true;
     }
 
-    private static IList<IConfigurationSection> GetWriteToSections(this IConfiguration configuration)
+    private static List<IConfigurationSection> GetWriteToSections(this IConfiguration configuration)
     {
         var serilogSection = configuration.GetSection("Serilog");
         if (serilogSection == null) return [];
 
-        return serilogSection.GetSection("WriteTo").GetChildren().ToList();
+        return serilogSection.GetSection(WriteToSectionName).GetChildren().ToList();
     }
     
-    private static IList<IConfigurationSection> GetWriteToExpressionSections(this IConfiguration configuration)
+    private static List<IConfigurationSection> GetWriteToExpressionSections(this IConfiguration configuration)
     {
         var writeToSections = configuration.GetWriteToSections();
-        return writeToSections.Select(writeTo => writeTo.GetSection("Args:configureLogger:Filter")).SelectMany(f => f.GetChildren())
-                .Select(c => c.GetSection("Args:expression")).ToList();
+        return [.. writeToSections.Select(writeTo => writeTo.GetSection($"Args:{ConfigureLoggerSectionName}:{FilterSectionName}"))
+            .SelectMany(f => f.GetChildren())
+            .Select(c => c.GetSection($"Args:{ExpressionSectionName}"))];
     }
-    
-    private static IList<IConfigurationSection> GetWriteToOutputTemplateSections(this IConfiguration configuration)
+
+    private static List<IConfigurationSection> GetWriteToExpressionSections(this IConfigurationSection writeToSection)
+    {
+        return [.. writeToSection.GetSection($"Args:{ConfigureLoggerSectionName}:{FilterSectionName}")
+            .GetChildren()
+            .Select(c => c.GetSection($"Args:{ExpressionSectionName}"))];
+    }
+
+    private static List<IConfigurationSection> GetWriteToOutputTemplateSections(this IConfiguration configuration)
     {
         var writeToSections = configuration.GetWriteToSections();
-        return writeToSections.SelectMany(writeTo => writeTo.GetSection("Args:configureLogger:WriteTo").GetChildren())
-               .Select(c => c.GetSection("Args:outputTemplate")).ToList();
+        return [.. writeToSections.SelectMany(writeTo => writeTo.GetSection($"Args:{ConfigureLoggerSectionName}:{WriteToSectionName}").GetChildren())
+            .Select(c => c.GetSection($"Args:{OutputTemplateSectionName}"))];
         //      .Where(e => e.Value != null && e.Value.Contains(PropertyNameContextStr)).ToList();
+    }
+
+    private static List<IConfigurationSection> GetWriteToOutputTemplateSections(this IConfigurationSection writeToSection)
+    {
+        return [.. writeToSection.GetSection($"Args:{ConfigureLoggerSectionName}:{WriteToSectionName}").
+            GetChildren().
+            Select(c => c.GetSection($"Args:{OutputTemplateSectionName}"))];
+    }
+
+    private static List<IConfigurationSection> GetWriteToSectionsByName(this IConfigurationSection writeToSection, string sectionName)
+    {
+        return [.. writeToSection.GetSection($"Args:{ConfigureLoggerSectionName}:{WriteToSectionName}").
+            GetChildren().
+            Select(c => c.GetSection($"Args:{sectionName}"))];
+    }
+
+    private static List<IConfigurationSection> GetWriteToPathSections(this IConfiguration configuration, string[] pathSectionNames)
+    {
+        var writeToSections = configuration.GetWriteToSections();
+        if (writeToSections.Count == 0) return [];
+
+        var pathes = new List<IConfigurationSection>();
+        foreach (var writeTo in writeToSections)
+        {
+            pathes.AddRange(pathSectionNames.SelectMany(s => writeTo.GetSection($"Args:{ConfigureLoggerSectionName}:{WriteToSectionName}")
+            .GetChildren()
+            .Select(c => c.GetSection($"Args:{s}"))));
+        }
+
+        return pathes;
+    }
+
+    private static List<IConfigurationSection> GetWriteToPathSections(this IConfigurationSection writeTo, string[] pathSectionNames)
+    {
+       return [.. pathSectionNames.SelectMany(s => writeTo.GetSection($"Args:{ConfigureLoggerSectionName}:{WriteToSectionName}")
+        .GetChildren()
+        .Select(c => c.GetSection($"Args:{s}")))];        
     }
 
     public static string BuildLogPath(this string appPath)
---
+++ Serilog.ContextFileSink/ContextPropertyFileSinkExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using Serilog.Sinks.File;
+using System.Text;
+
+namespace Serilog.ContextFileSink;
+
+public static class ContextPropertyFileSinkExtensions
+{
+    const int DefaultRetainedFileCountLimit = 31; // A long month of logs
+    const long DefaultFileSizeLimitBytes = 1L * 1024 * 1024 * 1024; // 1GB
+    const string DefaultOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+      
+    public static LoggerConfiguration ContextPropertyFile(
+        this LoggerSinkConfiguration sinkConfiguration,
+        string propertyName,
+        string pathFormat,
+        LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+        string outputTemplate = DefaultOutputTemplate,
+        IFormatProvider? formatProvider = null,
+        long? fileSizeLimitBytes = DefaultFileSizeLimitBytes,
+        LoggingLevelSwitch? levelSwitch = null,
+        bool buffered = false,
+        RollingInterval rollingInterval = RollingInterval.Infinite,
+        bool rollOnFileSizeLimit = false,
+        int? retainedFileCountLimit = DefaultRetainedFileCountLimit,
+        Encoding? encoding = null,
+        FileLifecycleHooks? hooks = null,
+        TimeSpan? retainedFileTimeLimit = null)
+    {
+        if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
+        if (pathFormat == null) throw new ArgumentNullException(nameof(pathFormat));
+        if (outputTemplate == null) throw new ArgumentNullException(nameof(outputTemplate));
+
+        var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+        var sink = new ContextPropertyFileSink(sinkConfiguration, propertyName,pathFormat, formatter,  restrictedToMinimumLevel, fileSizeLimitBytes,
+            levelSwitch, outputTemplate, encoding, buffered, hooks, rollingInterval,retainedFileCountLimit, rollOnFileSizeLimit, 
+             retainedFileTimeLimit);
+        return sinkConfiguration.Sink(sink);
+    }
+}
